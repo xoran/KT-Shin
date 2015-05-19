2c6fef4ec91180a655e727ac4fa33a1811f73340
Xoran <xoran@xoransorvor.be>
2015-05-19 20:25:26 +0200

    Update favicon

--- inc/configuration.php
+++ inc/configuration.php
@@ -28,7 +28,7 @@ if (is_file($GLOBALS['config']['DATADIR'].'/options.php')) require($GLOBALS['con
 
 
 define('shaarli_version','0.0.41 beta');
-define('KTSHIN_VERSION', '0.2.16 alpha');
+define('KTSHIN_VERSION', '0.2.17 alpha');
 define('LANG','fr');
 define('PHPPREFIX','<?php /* '); // Prefix to encapsulate data in php code.
 define('PHPSUFFIX',' */ ?>'); // Suffix to encapsulate data in php code.
